121ffd93590e80f96e8eff253477433c1253264f
Fabian Kuon <kuonfabian@freenet.de>
2020-06-21 12:41:13 +0200

    further development of bot

--- instabot.py
+++ instabot.py
@@ -1,49 +1,33 @@
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import getpass
 
 class InstagramBot(object):
 
-    def __init__(self, driver_settings, usr, pw):
+    def __init__(self, usr, pw):
         self.usr = usr
         self.pw = pw
-        self.driver_settings = driver_settings
-        self.driver_path, self.brave_path = self.Txt_File_to_String(driver_settings)
 
-        #Create object of ChromeOptions class
-        option = webdriver.ChromeOptions()
-        option.binary_location = self.driver_path
+        #Create a new instance of safari and open instagram
+        self.browser = webdriver.Safari()
+        self.browser.get('https://instagram.com')
+        self.browser.implicitly_wait(5)
+        self.insta_login()
+        self.destroy_session()
 
-        #Create a new instance of chrome
-        browser = webdriver.Chrome(executable_path = self.brave_path ,
-                                   chrome_options = option)
+    #Logging into instagram
+    def insta_login(self):
+        self.browser.find_element_by_name('username').send_keys(self.usr)
+        self.browser.find_element_by_name('password').send_keys(self.pw)
+        self.browser.find_element_by_xpath("//div[contains(text(), 'Log In')]")[0].click()
 
-        #Set the path of the brave browser
-        options.setBinary("Applications/Brave Browser.app")
-        self.browser.get('https://www.instagram.com')
-        self.browser.find_element_by_xpath("").send_keys(usr)
-        self.browser.find_element_by_xpath("//input[@name=\"password\"]").send_keys(pw)
-
-        self.browser.find_element_by_xpath('//button[@type="submit"]').click
-
-        self.slep(4)
+    def destroy_session(self):
         self.browser.quit()
 
-
-    def Txt_File_to_String(self, driver_settings):
-        data = []
-        with open(driver_settings) as f:
-            for line in f:
-                data.append(line.strip())
-        return data
-
-
-def main():
+if __name__ == "__main__":
     usr = input("Enter instagram username: \n")
     print("Enter your password \n")
     pw = getpass.getpass()
-    bot = InstagramBot('DriverSettings.txt', usr, pw)
-    bot.Txt_File_to_String()
+    bot = InstagramBot(usr, pw)
 
-if __name__ == "__main__":
-    main()
